187b2523bf4206a6f3c295e031a4c4db0987c77d
svolokitin <volokitin14sv@gmail.com>
2024-09-10 03:57:58 +0300

    сделал прослушку на кнопку, елементы div теперь нормально добавляються в блок сообщений, нужно разобраться с id_user на client-е

--- front/index.js
+++ front/index.js
@@ -2,22 +2,51 @@ import { io } from "https://cdn.socket.io/4.7.5/socket.io.esm.min.js";
 
 const socket = io('http://localhost:3000');
 
+const chat__window = document.getElementById('chat__window');
+const chat__message = document.getElementById('chat__message');
+const send__button = document.getElementById('send__button');
+
+// функция отправки сообщений в окно чата
+
+function sendMessage (message) {
+    const chat__message__text = message;
+    const user_id = socket.id;
+
+    if (chat__message__text.trim()) {
+        const chat__message__element = document.createElement('div');
+
+        chat__message__element.id = 'chat__message__element';
+        chat__message__element.textContent = `${user_id}  ${chat__message__text}`;
+
+        chat__window.appendChild(chat__message__element);
+
+        chat__message.value = '';
+
+        chat__window.scrollTop = chat__window.scrollHeight;
+    }
+}
+
 // Обработка сообщений
-socket.on('message', (message) => {
-    const chat__window = document.getElementById('chat__window');
-    let div = document.createElement('div');
-    div.className = '#777';
-    div.innerHTML = `<p>${message}</p>`;
-    
+socket.on('message', (message) => {  
+    sendMessage(message);
 });
 
-// Прослушка кнопки
+// по клику
 
-document.getElementById('send__button').onclick = () => {
-    const message = document.getElementById('chat__message').value;
+send__button.addEventListener('click', function() {
+    const chat__message__text = document.getElementById('chat__message').value;
 
-    document.getElementById('chat__window').innerText = message;
-    socket.send(message);
-}
+    socket.send(chat__message__text);
+});
+
+// по нажатию
+
+chat__message.addEventListener('keydown', function (event) {
+    if (event.key == 'Enter') {
+        const chat__message__text = document.getElementById('chat__message').value;
+
+        socket.send(chat__message__text);
+    }
+});
 
 
